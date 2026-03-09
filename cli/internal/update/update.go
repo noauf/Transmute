0e125e7b1ab9c5d0c0560ece73a2bb5aa7e1495b
noah <auf230184@spengergasse.at>
2026-03-09 23:15:12 +0100

    bump: v0.1.1 for TUI background fix release

--- cli/internal/update/update.go
+++ cli/internal/update/update.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	// CurrentVersion is the embedded build version. Updated at release time.
-	CurrentVersion = "0.1.0"
+	CurrentVersion = "0.1.1"
 
 	repoOwner = "noauf"
 	repoName  = "Transmute"
